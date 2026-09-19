f5790060672d07ce0eea064200a95468db0246ab
laxmi25-hub <laxmi25@navgurukul.org>
2026-09-19 16:20:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// 1). let a = 20;
+// let b = 50;
+// console.log(a + b);
+
+// 2). let Orange = 90;
+// let Apple = 20;
+// console.log(Orange - Apple);
+
+// 3). let festName  = "Laxmi";
+// let letsName = "Bai";
+// console.log(festName + " " + letsName);
+
+
+// 4). let Number = 2345;
+// let Numbers = 6543;
+// console.log(Number > Numbers);
+
+
+// 4). let Number = 2345;
+// let Numbers = 6543;
+// console.log(Number < Numbers);
+
+// 5). let Number = -30;
+// if (Number > 0){
+//     console.log("Positive")
+// }else if (Number < 0){
+//     console.log("Negative")
+// }else{
+//     console.log("Zoro");
+// }
